707ea8e902d21227632dbbe5f3b9d75be9453376
Rodrigo <rduclerc@gmail.com>
2020-03-08 12:26:09 -0300

    Implementação inicial do DependencyContainer

---
+++ DesafioMundiPagg2020/MundiPagg.Infra.IoC/DepedencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MundiPagg.Domain.Core.Bus;
+using MundiPagg.Infra.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MundiPagg.Infra.IoC
+{
+    public class DepedencyContainer
+    {
+        static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
