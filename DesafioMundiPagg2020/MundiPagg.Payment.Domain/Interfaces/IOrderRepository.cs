8236c0259e9fdee7f06905768eda995f90c3f9ee
Rodrigo <rduclerc@gmail.com>
2020-03-08 13:25:10 -0300

    Criação da interface IOrderRepository

---
+++ DesafioMundiPagg2020/MundiPagg.Payment.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using MundiPagg.Payment.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MundiPagg.Payment.Domain.Interfaces
+{
+    public interface IOrderRepository
+    {
+        IEnumerable<Order> GetOrders();
+        IEnumerable<Order> AddOrder(Order order);
+    }
+}
