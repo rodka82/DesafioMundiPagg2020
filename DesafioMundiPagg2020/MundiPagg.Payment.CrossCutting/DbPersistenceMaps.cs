ee9d822f78de1864b1edb8d929181af6d652c8c6
Rodrigo <rduclerc@gmail.com>
2020-03-13 02:24:59 -0300

    Criação da classe agregadora dos mappings

---
+++ DesafioMundiPagg2020/MundiPagg.Payment.CrossCutting/DbPersistenceMaps.cs
@@ -0,0 +1,12 @@
+﻿using MundiPagg.Payment.Data.Mapping;
+
+namespace MundiPagg.Payment.CrossCutting
+{
+    public class DbPersistenceMaps
+    {
+        public static void Configure()
+        {
+            OrderRequestMap.Configure();
+        }
+    }
+}
