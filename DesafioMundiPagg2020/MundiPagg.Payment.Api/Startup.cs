006c6c713b7787da4457faec996b2a5413d45290
Rodrigo <rduclerc@gmail.com>
2020-03-13 03:41:02 -0300

    Configurações do startup e conexão com banco

--- DesafioMundiPagg2020/MundiPagg.Payment.Api/Startup.cs
+++ DesafioMundiPagg2020/MundiPagg.Payment.Api/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,7 +12,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using MundiPagg.Infra.IoC;
 using MundiPagg.Payment.CrossCutting;
+using MundiPagg.Payment.Data.Context;
 
 namespace MundiPagg.Payment.Api
 {
@@ -27,6 +31,23 @@ namespace MundiPagg.Payment.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(option => option.EnableEndpointRouting = false);
+
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Payment Microservice",
+                    Version = "v1"
+                });
+            });
+
+            services.AddMediatR(typeof(Startup));
+
+            RegisterServices(services);
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            DepedencyContainer.RegisterServices(services);
         }
 
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
@@ -41,7 +62,24 @@ namespace MundiPagg.Payment.Api
             }
 
             app.UseHttpsRedirection();
+
+            ConfigureDataBase();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json/", "Payment Microservice v1");
+            });
+
             app.UseMvc();
+
+        }
+
+        private void ConfigureDataBase()
+        {
+            PaymentDbContext.ConnectionString = Configuration.GetSection("MongoConnection:ConnectionString").Value;
+            PaymentDbContext.DatabaseName = Configuration.GetSection("MongoConnection:Database").Value;
+            PaymentDbContext.IsSSL = Convert.ToBoolean(this.Configuration.GetSection("MongoConnection:IsSSL").Value);
 
             DbPersistenceMaps.Configure();
         }
