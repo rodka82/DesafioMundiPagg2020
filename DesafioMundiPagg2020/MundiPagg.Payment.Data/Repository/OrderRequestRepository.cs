887d32c2ce939b9b6d9dfa62768c6de1de600ad7
Rodrigo <rduclerc@gmail.com>
2020-03-13 02:22:19 -0300

    Criação de OrderRequestRepository

---
+++ DesafioMundiPagg2020/MundiPagg.Payment.Data/Repository/OrderRequestRepository.cs
@@ -0,0 +1,28 @@
+﻿using MundiPagg.Payment.Data.Context;
+using MundiPagg.Payment.Domain.Interfaces;
+using MundiPagg.Payment.Domain.Models;
+using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+
+namespace MundiPagg.Payment.Data.Repository
+{
+    public class OrderRequestRepository : IOrderRepository
+    {
+        private PaymentDbContext _context;
+
+        public OrderRequestRepository(PaymentDbContext context)
+        {
+            _context = context;
+        }
+        public void AddOrder(OrderRequest order)
+        {
+            _context.Orders.InsertOne(order);
+        }
+
+        public IEnumerable<OrderRequest> GetOrders()
+        {
+            return _context.Orders.Find(m => true).ToList();
+        }
+    }
+}
