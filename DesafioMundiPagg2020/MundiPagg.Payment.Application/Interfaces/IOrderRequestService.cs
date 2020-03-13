fdb3d36138abd6654c36138bbf8a9a6257b7ff2d
Rodrigo <rduclerc@gmail.com>
2020-03-13 03:39:49 -0300

    Ajustes em OrderRequestService

---
+++ DesafioMundiPagg2020/MundiPagg.Payment.Application/Interfaces/IOrderRequestService.cs
@@ -0,0 +1,13 @@
+﻿using MundiPagg.Payment.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MundiPagg.Payment.Application.Interfaces
+{
+    public interface IOrderRequestService
+    {
+        void AddOrderRequest(OrderRequest orderRequest);
+        IEnumerable<OrderRequest> GetOrderRequests();
+    }
+}
---
+++ DesafioMundiPagg2020/MundiPagg.Payment.Application/Services/OrderRequestService.cs
@@ -0,0 +1,33 @@
+﻿using MundiPagg.Domain.Core.Bus;
+using MundiPagg.Payment.Application.Interfaces;
+using MundiPagg.Payment.Data.Repository;
+using MundiPagg.Payment.Domain.Interfaces;
+using MundiPagg.Payment.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MundiPagg.Payment.Application.Services
+{
+    public class OrderRequestService : IOrderRequestService
+    {
+        private readonly IOrderRequestRepository _orderRequestRepository;
+
+        private readonly IEventBus _bus;
+
+        public OrderRequestService(IOrderRequestRepository orderRequestRepository)
+        {
+            _orderRequestRepository = orderRequestRepository;
+        }
+
+        public void AddOrderRequest(OrderRequest orderRequest)
+        {
+            _orderRequestRepository.AddOrderRequest(orderRequest);
+        }
+
+        public IEnumerable<OrderRequest> GetOrderRequests()
+        {
+            return _orderRequestRepository.GetOrderRequests();
+        }
+    }
+}
