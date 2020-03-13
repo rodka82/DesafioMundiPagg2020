e2aca79ebf037ee9995486a287fe457b0a0fb211
Rodrigo <rduclerc@gmail.com>
2020-03-13 02:23:44 -0300

    Criação do Mapping de OrderRequest

---
+++ DesafioMundiPagg2020/MundiPagg.Payment.Data/Mapping/OrderRequestMap.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization;
+using MundiPagg.Payment.Domain.Models;
+
+namespace MundiPagg.Payment.Data.Mapping
+{
+    public class OrderRequestMap
+    {
+        public static void Configure()
+        {
+            BsonClassMap.RegisterClassMap<OrderRequest>(map =>
+            {
+                map.AutoMap();
+                map.SetIgnoreExtraElements(true);
+                map.MapIdMember(x => x.Id);
+                map.MapMember(x => x.Request).SetIsRequired(true);
+                map.MapMember(x => x.RequestDate).SetIsRequired(true);
+                map.MapMember(x => x.Response).SetIsRequired(true);
+                map.MapMember(x => x.ResponseDate).SetIsRequired(true);
+            });
+        }
+    }
+}
