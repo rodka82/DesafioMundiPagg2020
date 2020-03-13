ea1ccc888edb251486baa3f9ea0925a8f8fc432e
Rodrigo <rduclerc@gmail.com>
2020-03-13 02:21:14 -0300

    Criação da Classe OrderRequest

---
+++ DesafioMundiPagg2020/MundiPagg.Payment.Domain/Models/OrderRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MundiPagg.Payment.Domain.Models
+{
+    public class OrderRequest
+    {
+        public int Id { get; set; }
+        public string Request { get; set; }
+        public DateTime RequestDate { get; set; }
+        public string Response { get; set; }
+        public DateTime ResponseDate { get; set; }
+     }
+}
