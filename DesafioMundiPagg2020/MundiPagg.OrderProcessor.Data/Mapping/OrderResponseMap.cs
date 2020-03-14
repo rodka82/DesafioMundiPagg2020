687a61a789e8d080e23ce1b661179af7ba540b58
Rodrigo <rduclerc@gmail.com>
2020-03-14 15:44:24 -0300

    Mapping de orderResponse

---
+++ DesafioMundiPagg2020/MundiPagg.OrderProcessor.Data/Mapping/OrderResponseMap.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization;
+using MundiPagg.OrderProcessor.Domain.Models;
+
+namespace MundiPagg.OrderProcessor.Data.Mapping
+{
+    public class OrderResponseMap
+    {
+        public static void Configure()
+        {
+            BsonClassMap.RegisterClassMap<OrderResponse>(map =>
+            {
+                map.AutoMap();
+                map.SetIgnoreExtraElements(true);
+                map.MapIdMember(x => x.Id);
+                map.MapMember(x => x.Request).SetIsRequired(true);
+                map.MapMember(x => x.RequestDate).SetIsRequired(true);
+                map.MapMember(x => x.Response).SetIsRequired(true);
+                map.MapMember(x => x.ResponseDate).SetIsRequired(true);
+            });
+        }
+    }
+}
