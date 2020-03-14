2ded4f55fb41c86a74edd246caaf23e898de61ec
Rodrigo <rduclerc@gmail.com>
2020-03-14 15:45:36 -0300

    Implesmentação de OrderResponseRepository

---
+++ DesafioMundiPagg2020/MundiPagg.OrderProcessor.Data/Repository/OrderResponseRepository.cs
@@ -0,0 +1,28 @@
+﻿using MundiPagg.OrderProcessor.Data.Context;
+using MundiPagg.OrderProcessor.Domain.Interfaces;
+using MundiPagg.OrderProcessor.Domain.Models;
+using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+
+namespace MundiPagg.OrderProcessor.Data.Repository
+{
+    public class OrderResponseRepository : IOrderResponseRepository
+    {
+        private OrderProcessorDbContext _context;
+
+        public OrderResponseRepository(OrderProcessorDbContext context)
+        {
+            _context = context;
+        }
+        public void AddOrderRequest(OrderResponse order)
+        {
+            _context.OrderResponses.InsertOne(order);
+        }
+
+        public IEnumerable<OrderResponse> GetOrderResponses()
+        {
+            return _context.OrderResponses.Find(m => true).ToList();
+        }
+    }
+}
