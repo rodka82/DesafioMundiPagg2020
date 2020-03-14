75f2a7d0f588d735f076f1beb50bb085e8b6d828
Rodrigo <rduclerc@gmail.com>
2020-03-14 17:14:27 -0300

    OrderREquestEventHandler adicionado

---
+++ DesafioMundiPagg2020/MundiPagg.OrderProcessor.Domain/EventHandlers/OrderRequestEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using MundiPagg.Domain.Core.Bus;
+using MundiPagg.OrderProcessor.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MundiPagg.OrderProcessor.Domain.EventHandlers
+{
+    public class OrderRequestEventHandler : IEventHandler<OrderRequestCreatedEvent>
+    {
+        public OrderRequestEventHandler()
+        {
+
+        }
+        public Task Handle(OrderRequestCreatedEvent @event)
+        {
+            //Enviar requisição para API da MundiPagg
+            return Task.CompletedTask;
+        }
+    }
+}
