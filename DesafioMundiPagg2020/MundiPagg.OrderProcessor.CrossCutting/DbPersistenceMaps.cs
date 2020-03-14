6aac066b9e4db1c720ceaaadbe0968af631eca7d
Rodrigo <rduclerc@gmail.com>
2020-03-14 15:43:53 -0300

    Mapeamento do documento do banco de dados e cross cutting do microservicço order processor

---
+++ DesafioMundiPagg2020/MundiPagg.OrderProcessor.CrossCutting/DbPersistenceMaps.cs
@@ -0,0 +1,12 @@
+﻿using MundiPagg.OrderProcessor.Data.Mapping;
+
+namespace MundiPagg.OrderProcessor.CrossCutting
+{
+    public class DbPersistenceMaps
+    {
+        public static void Configure()
+        {
+            OrderResponseMap.Configure();
+        }
+    }
+}
