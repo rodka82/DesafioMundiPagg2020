436d9bcd4ec1e798aacea5274168933c305fb285
Rodrigo <rduclerc@gmail.com>
2020-03-14 17:15:10 -0300

    Criação de classe ApiOrderConverter

---
+++ DesafioMundiPagg2020/MundiPagg.Payment.Application/Services/ApiOrderConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace MundiPagg.Payment.Application.Services
+{
+    public class ApiOrderConverter : JsonConverter
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return objectType.GetTypeInfo().IsClass;
+        }
+    }
+}
