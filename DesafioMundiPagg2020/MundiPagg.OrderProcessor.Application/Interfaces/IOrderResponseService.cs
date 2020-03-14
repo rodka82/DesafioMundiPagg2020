5107c9557cc2c901e8f95b9a4acb719ec2412f83
Rodrigo <rduclerc@gmail.com>
2020-03-14 15:43:14 -0300

    Interface e implementação de IOrderResponseService

---
+++ DesafioMundiPagg2020/MundiPagg.OrderProcessor.Application/Interfaces/IOrderResponseService.cs
@@ -0,0 +1,12 @@
+﻿using MundiPagg.OrderProcessor.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MundiPagg.OrderProcessor.Application.Interfaces
+{
+    public interface IOrderResponseService
+    {
+        IEnumerable<OrderResponse> GetOrderRequests();
+    }
+}
---
+++ DesafioMundiPagg2020/MundiPagg.OrderProcessor.Application/Services/OrderResponseService.cs
@@ -0,0 +1,28 @@
+﻿using MundiPagg.Domain.Core.Bus;
+using MundiPagg.OrderProcessor.Application.Interfaces;
+using MundiPagg.OrderProcessor.Domain.Interfaces;
+using MundiPagg.OrderProcessor.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MundiPagg.OrderProcessor.Application.Services
+{
+    public class OrderResponseService : IOrderResponseService
+    {
+        private readonly IOrderResponseRepository _orderResponseRepository;
+
+        private readonly IEventBus _bus;
+
+        public OrderResponseService(IOrderResponseRepository orderRequestRepository, IEventBus bus)
+        {
+            _orderResponseRepository = orderRequestRepository;
+            _bus = bus;
+        }
+
+        public IEnumerable<OrderResponse> GetOrderRequests()
+        {
+            return _orderResponseRepository.GetOrderResponses();
+        }
+    }
+}
