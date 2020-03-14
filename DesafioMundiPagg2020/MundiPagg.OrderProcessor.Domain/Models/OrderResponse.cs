18983d325e078dcda8d7347c9376f28226679c6f
Rodrigo <rduclerc@gmail.com>
2020-03-14 15:46:02 -0300

    Criação da model OrderResponse

---
+++ DesafioMundiPagg2020/MundiPagg.OrderProcessor.Domain/Models/OrderResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MundiPagg.OrderProcessor.Domain.Models
+{
+    public class OrderResponse
+    {
+        public int Id { get; set; }
+        public string Request { get; set; }
+        public DateTime RequestDate { get; set; }
+        public string Response { get; set; }
+        public DateTime ResponseDate { get; set; }
+     }
+}
