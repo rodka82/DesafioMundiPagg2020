add8264c78dd1eeb9d41bc040f3fca4fbd413faa
Rodrigo <rduclerc@gmail.com>
2020-03-14 15:41:59 -0300

    Criação da controller de OrderProcessor

---
+++ DesafioMundiPagg2020/MundiPagg.OrderProcessor.Api/Controllers/OrderProcessorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MundiPagg.OrderProcessor.Application.Interfaces;
+using MundiPagg.OrderProcessor.Domain.Models;
+
+namespace MundiPagg.OrderProcessor.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderProcessorController : ControllerBase
+    {
+        private readonly IOrderResponseService _orderResponseService;
+
+        public OrderProcessorController(IOrderResponseService orderResponseService)
+        {
+            _orderResponseService = orderResponseService;
+        }
+        // GET api/orderProcessor
+        [HttpGet]
+        public ActionResult<IEnumerable<OrderResponse>> Get()
+        {
+            return Ok(_orderResponseService.GetOrderRequests());
+        }
+    }
+}
