542b3ae9c9e228ecacdfd047d0b42d1963f775c2
Rodrigo <rduclerc@gmail.com>
2020-03-14 15:45:49 -0300

    Interface de order response repository

---
+++ DesafioMundiPagg2020/MundiPagg.OrderProcessor.Domain/Interfaces/IOrderResponseRepository.cs
@@ -0,0 +1,13 @@
+﻿using MundiPagg.OrderProcessor.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MundiPagg.OrderProcessor.Domain.Interfaces
+{
+    public interface IOrderResponseRepository
+    {
+        IEnumerable<OrderResponse> GetOrderResponses();
+        void AddOrderRequest(OrderResponse order);
+    }
+}
