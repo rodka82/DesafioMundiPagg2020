b8d9add32b2b10e6bced54484cbb3b8831b7856a
Rodrigo <rduclerc@gmail.com>
2020-03-14 15:44:10 -0300

    Criação do dbContext de OrderProcessor

---
+++ DesafioMundiPagg2020/MundiPagg.OrderProcessor.Data/Context/OrderProcessorDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Driver;
+using MundiPagg.OrderProcessor.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MundiPagg.OrderProcessor.Data.Context
+{
+    public class OrderProcessorDbContext : DbContext
+    {
+        public static string ConnectionString { get; set; }
+        public static string DatabaseName { get; set; }
+        public static bool IsSSL { get; set; }
+        private IMongoDatabase _database { get; }
+
+        public OrderProcessorDbContext()
+        {
+            try
+            {
+                MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(ConnectionString));
+                if (IsSSL)
+                {
+                    settings.SslSettings = new SslSettings { EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12 };
+                }
+                var mongoClient = new MongoClient(settings);
+                _database = mongoClient.GetDatabase(DatabaseName);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Não foi possível se conectar com o servidor.", ex);
+            }
+        }
+
+        public IMongoCollection<OrderResponse> OrderResponses
+        {
+            get
+            {
+                return _database.GetCollection<OrderResponse>("Order");
+            }
+        }
+    }
+}
