0354d9621d47355a48699a1c18fcf74d570ec43c
Rodrigo <rduclerc@gmail.com>
2020-03-15 18:40:58 -0300

    Ajuste nomenclatura

--- DesafioMundiPagg2020/MundiPagg.Tests.Payment/ApiOrderConverterTests.cs
+++ DesafioMundiPagg2020/MundiPagg.Tests.Payment/ApiOrderConverterTests.cs
@@ -46,29 +46,18 @@ namespace MundiPagg.Tests.Payment
         [Fact]
         public void ShouldConvertJsonToSpecifiedFormat()
         {
-            string jsonFromBank = "{\"numero_pedido\":\"OR1234\",\"comprador\":{\"aniversario\":\"1991-05-20T00:00:00\",\"documento\":\"12345678900\",\"email\":\"fulano@gmail.com\",\"telefone\":\"552122225555\",\"celular\":\"5521999995555\",\"nome\":\"Fulano Silva\",\"tipo\":\"pessoa_fisica\",\"endereco\":{\"cidade\":\"Rio de Janeiro\",\"complemento\":\"Apto 1011\",\"pais\":\"Brazil\",\"bairro\":\"Centro\",\"numero\":\"123\",\"estado\":\"RJ\",\"logradouro\":\"Rua Souza\",\"cep\":\"23021130\"}},\"pagamento\":{\"valor\":210,\"parcelas\":\"5\",\"endereco_cobranca\":{\"cidade\":\"Rio de Janeiro\",\"complemento\":\"Apto 1011\",\"pais\":\"Brazil\",\"bairro\":\"Centro\",\"numero\":\"123\",\"estado\":\"RJ\",\"logradouro\":\"Rua Souza\",\"cep\":\"23021130\"},\"cartao\":{\"bandeira\":\"Visa\",\"numero_cartao\":\"4000000000000010\",\"mes_vencimento\":\"10\",\"ano_vencimento\":\"2022\",\"nome_cartao\":\"FULANO SILVA\",\"cvv\":\"321\"}},\"entrega\":{\"endereco_entrega\":{\"cidade\":\"Rio de Janeiro\",\"complemento\":\"Apto 1011\",\"pais\":\"Brazil\",\"bairro\":\"Centro\",\"numero\":\"123\",\"estado\":\"RJ\",\"logradouro\":\"Rua Souza\",\"cep\":\"23021130\"},\"frete\":10,\"shipping_company\":\"Correios\"},\"carriinho\":{\"items\":[{\"produto\":\"Discos Vinil\",\"quantidade\":10,\"valor_unit\":10},{\"produto\":\"Toca Viniil\",\"quantidade\":1,\"valor_unit\":100}]}}";
+            string jsonFromDb = "{\"numero_pedido\":\"OR1234\",\"comprador\":{\"aniversario\":\"1991-05-20T00:00:00\",\"documento\":\"12345678900\",\"email\":\"fulano@gmail.com\",\"telefone\":\"552122225555\",\"celular\":\"5521999995555\",\"nome\":\"Fulano Silva\",\"tipo\":\"pessoa_fisica\",\"endereco\":{\"cidade\":\"Rio de Janeiro\",\"complemento\":\"Apto 1011\",\"pais\":\"Brazil\",\"bairro\":\"Centro\",\"numero\":\"123\",\"estado\":\"RJ\",\"logradouro\":\"Rua Souza\",\"cep\":\"23021130\"}},\"pagamento\":{\"valor\":210,\"parcelas\":\"5\",\"endereco_cobranca\":{\"cidade\":\"Rio de Janeiro\",\"complemento\":\"Apto 1011\",\"pais\":\"Brazil\",\"bairro\":\"Centro\",\"numero\":\"123\",\"estado\":\"RJ\",\"logradouro\":\"Rua Souza\",\"cep\":\"23021130\"},\"cartao\":{\"bandeira\":\"Visa\",\"numero_cartao\":\"4000000000000010\",\"mes_vencimento\":\"10\",\"ano_vencimento\":\"2022\",\"nome_cartao\":\"FULANO SILVA\",\"cvv\":\"321\"}},\"entrega\":{\"endereco_entrega\":{\"cidade\":\"Rio de Janeiro\",\"complemento\":\"Apto 1011\",\"pais\":\"Brazil\",\"bairro\":\"Centro\",\"numero\":\"123\",\"estado\":\"RJ\",\"logradouro\":\"Rua Souza\",\"cep\":\"23021130\"},\"frete\":10,\"shipping_company\":\"Correios\"},\"carriinho\":{\"items\":[{\"produto\":\"Discos Vinil\",\"quantidade\":10,\"valor_unit\":10},{\"produto\":\"Toca Viniil\",\"quantidade\":1,\"valor_unit\":100}]}}";
             string jsonFromRequest = "{\"numero_pedido\":\"OR1235\",\"comprador\":{\"aniversario\":\"1991-05-20T00:00:00\",\"documento\":\"12345678900\",\"email\":\"fulano@gmail.com\",\"telefone\":\"552122225555\",\"celular\":\"5521999995555\",\"nome\":\"Fulano Silva\",\"tipo\":\"pessoa_fisica\",\"endereco\":{\"cidade\":\"Rio de Janeiro\",\"complemento\":\"Apto 1011\",\"pais\":\"Brazil\",\"bairro\":\"Centro\",\"numero\":\"123\",\"estado\":\"RJ\",\"logradouro\":\"Rua Souza\",\"cep\":\"23021130\"}},\"pagamento\":{\"valor\":210,\"parcelas\":\"5\",\"endereco_cobranca\":{\"cidade\":\"Rio de Janeiro\",\"complemento\":\"Apto 1011\",\"pais\":\"Brazil\",\"bairro\":\"Centro\",\"numero\":\"123\",\"estado\":\"RJ\",\"logradouro\":\"Rua Souza\",\"cep\":\"23021130\"},\"cartao\":{\"bandeira\":\"Visa\",\"numero_cartao\":\"4000000000000010\",\"mes_vencimento\":\"10\",\"ano_vencimento\":\"2022\",\"nome_cartao\":\"FULANO SILVA\",\"cvv\":\"321\"}},\"entrega\":{\"endereco_entrega\":{\"cidade\":\"Rio de Janeiro\",\"complemento\":\"Apto 1011\",\"pais\":\"Brazil\",\"bairro\":\"Centro\",\"numero\":\"123\",\"estado\":\"RJ\",\"logradouro\":\"Rua Souza\",\"cep\":\"23021130\"},\"frete\":10,\"shipping_company\":\"Correios\"},\"carriinho\":{\"items\":[{\"produto\":\"Discos Vinil\",\"quantidade\":10,\"valor_unit\":10},{\"produto\":\"Toca Viniil\",\"quantidade\":1,\"valor_unit\":100}]}}";
 
-            Assert.True(jsonFromRequest.HasSameKeys(jsonFromBank));
+            Assert.True(jsonFromRequest.HasSameKeys(jsonFromDb));
 
             var jsonDataDictionary = jsonFromRequest.GetKeyAndValuesToDictionary();
 
-            //o json da requisição pode ser mapeado para MundiPaggOrder.Customer
             Assert.True(jsonDataDictionary.canBeMappedTo(_customerPropertyMappings));
-
-            //o json da requisição pode ser mapeado para MundiPaggOrder.Itens
             Assert.True(jsonDataDictionary.canBeMappedTo(_itemPropertyMappings));
-
-            //o json da requisição pode ser mapeado para MundiPaggOrder.Payments
             Assert.True(jsonDataDictionary.canBeMappedTo(_paymentPropertyMappings));
 
-            //Instancia MundiPaggOrder para preencher
-            var mundiPaggOrder = new MundiPaggOrder
-            {
-                Customer = new Customer(),
-                Itens = new List<Item>(),
-                Payments = new List<MundiPagg.Payment.Application.Models.Payment>()
-            };
+            var mundiPaggOrder = new MundiPaggOrder();
 
             var payment = PaymentFactory.Create(jsonDataDictionary, _paymentPropertyMappings);
             mundiPaggOrder.Payments.Add(payment);
@@ -76,20 +65,10 @@ namespace MundiPagg.Tests.Payment
             var customer = CustomerFactory.Create(jsonDataDictionary, _customerPropertyMappings);
             mundiPaggOrder.Customer = customer;
 
-            var itensRequest = jsonDataDictionary
-                .Where(i => _itemPropertyMappings.Any(k => i.Key.RemoveBrackets() == k.Key))
-                .GroupBy(o => o.Key.Substring(0, o.Key.IndexOf(']')))
-                .ToDictionary(g => g.Key, g => g.ToList())
-                .Select(r => new Item
-                {
-                    Description = r.Value[0].Value,
-                    Quantity = Convert.ToInt32(r.Value[1].Value),
-                    Amount = Convert.ToDouble(r.Value[2].Value)
-                });
+            var itens = ItensFactory.CreateList(jsonDataDictionary, _itemPropertyMappings);
+            mundiPaggOrder.Itens = itens;
 
-            foreach (var item in itensRequest)
-                mundiPaggOrder.Itens.Add(item);
-
+            Assert.True(mundiPaggOrder.Itens.Any() && mundiPaggOrder.Payments.Any() && !string.IsNullOrEmpty(mundiPaggOrder.Customer.Name));
         }
     }
 }
