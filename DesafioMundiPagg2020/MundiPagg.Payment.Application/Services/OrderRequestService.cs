1bc4ff3e6ef5bf0cb9f7b246aaa57528e23e6c49
Rodrigo <rduclerc@gmail.com>
2020-03-15 21:46:33 -0300

    Criação de novo serviço

--- DesafioMundiPagg2020/MundiPagg.Payment.Application/Services/OrderRequestService.cs
+++ DesafioMundiPagg2020/MundiPagg.Payment.Application/Services/OrderRequestService.cs
@@ -6,13 +6,45 @@ using MundiPagg.Payment.Domain.Interfaces;
 using MundiPagg.Payment.Domain.Models;
 using System;
 using System.Collections.Generic;
+using MundiPagg.Payment.Application.Factories;
 using System.Text;
+using MundiPagg.Payment.Application.Extensions;
+using MundiPagg.Payment.Application.Models;
+using Newtonsoft.Json;
 
 namespace MundiPagg.Payment.Application.Services
 {
     public class OrderRequestService : IOrderRequestService
     {
         private readonly IOrderRequestRepository _orderRequestRepository;
+        private Dictionary<string, string> _orderPropertyMappings = new Dictionary<string, string>
+        {
+            {"numero_pedido","Code"}
+        };
+        private Dictionary<string, string> _customerPropertyMappings = new Dictionary<string, string>
+            {
+                {"comprador.email","Email"},
+                {"comprador.nome","Name"},
+            };
+        private Dictionary<string, string> _itemPropertyMappings = new Dictionary<string, string>
+            {
+                {"carriinho.items.produto", "Description"},
+                {"carriinho.items.quantidade", "Quantity"},
+                {"carriinho.items.valor_unit", "Amount"}
+            };
+        private Dictionary<string, string> _paymentPropertyMappings = new Dictionary<string, string>
+            {
+                {"pagamento.parcelas","Installments"},
+                {"pagamento.endereco_cobranca.pais","Country"},
+                {"pagamento.endereco_cobranca.estado","State"},
+                {"pagamento.endereco_cobranca.logradouro","Line1"},
+                {"pagamento.endereco_cobranca.cep","Zipcode"},
+                {"pagamento.cartao.numero_cartao","Number"},
+                {"pagamento.cartao.mes_vencimento","ExpMonth"},
+                {"pagamento.cartao.ano_vencimento","ExpYear"},
+                {"pagamento.cartao.nome_cartao","HolderName"},
+                {"pagamento.cartao.cvv","Cvv"}
+            };
 
         private readonly IEventBus _bus;
 
@@ -27,11 +59,14 @@ namespace MundiPagg.Payment.Application.Services
             _orderRequestRepository.AddOrderRequest(orderRequest);
         }
 
-        public void CreateOrder(OrderRequest orderRequest)
+        public void CreateOrder(string orderRequestJson)
         {
+            MundiPaggOrder mundiPaggOrder = MundiPaggOrderFactory.Create(orderRequestJson.GetKeyAndValuesToDictionary(), _customerPropertyMappings, _paymentPropertyMappings, _itemPropertyMappings, _orderPropertyMappings);
+            var orderRequest = OrderRequestFactory.Create(mundiPaggOrder);
+
             var createOrderRequestCommand = new CreateOrderRequestCommand(
                 orderRequest.Id,
-                orderRequest.RequestDate);
+                DateTime.Now);
 
             _bus.SendCommand(createOrderRequestCommand);
             AddOrderRequest(orderRequest);
