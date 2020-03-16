fe2e18df9ab06fee92a6bbc7d4a7b1db43701205
Rodrigo <rduclerc@gmail.com>
2020-03-15 21:45:28 -0300

    Ajuste em orderREquest

--- DesafioMundiPagg2020/MundiPagg.Payment.Domain/Models/OrderRequest.cs
+++ DesafioMundiPagg2020/MundiPagg.Payment.Domain/Models/OrderRequest.cs
@@ -4,7 +4,7 @@ namespace MundiPagg.Payment.Domain.Models
 {
     public class OrderRequest
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Request { get; set; }
         public DateTime RequestDate { get; set; }
         public string Response { get; set; }
