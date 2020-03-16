19e15ca591c8cba28fcf554cde51a2226cf68603
Rodrigo <rduclerc@gmail.com>
2020-03-15 21:45:54 -0300

    Implementação de repositorio

--- DesafioMundiPagg2020/MundiPagg.OrderProcessor.Data/Repository/OrderResponseRepository.cs
+++ DesafioMundiPagg2020/MundiPagg.OrderProcessor.Data/Repository/OrderResponseRepository.cs
@@ -15,14 +15,25 @@ namespace MundiPagg.OrderProcessor.Data.Repository
         {
             _context = context;
         }
-        public void AddOrderRequest(OrderResponse order)
+        public void AddOrderResponse(OrderResponse order)
         {
             _context.OrderResponses.InsertOne(order);
+        }
+
+        public OrderResponse GetOrderResponseById(int id)
+        {
+            var orderResponse = _context.OrderResponses.Find(m => m.Id == id).FirstOrDefault();
+            return orderResponse;
         }
 
         public IEnumerable<OrderResponse> GetOrderResponses()
         {
             return _context.OrderResponses.Find(m => true).ToList();
+        }
+
+        public void UpdateOrderResponse(OrderResponse order)
+        {
+            _context.OrderResponses.ReplaceOne(m => m.Id == order.Id, order);
         }
     }
 }
